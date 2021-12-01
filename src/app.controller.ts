242526b02e282dd5a2fdfb03bf150e5c3cf06901
SunsetFrost <1262457596@qq.com>
2021-12-01 11:11:58 +0800

    feat:add env config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const key = this.configService.get<string>('TMDB_API_KEY');
+    const test = this.appService.getHello();
+    return test + key;
   }
 }
