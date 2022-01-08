f769fe1aa0f63338f26b12747f73d83d551954ac
SunsetFrost <1262457596@qq.com>
2022-01-08 15:25:42 +0800

    fix:some bug

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,5 +1,12 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
-import { Observable, forkJoin, switchMap } from 'rxjs';
+import {
+  Observable,
+  forkJoin,
+  concatMap,
+  switchMap,
+  mergeMap,
+  map,
+} from 'rxjs';
 import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
@@ -10,17 +17,46 @@ export class PokemonController {
   getList(@Query('page') page: number): Observable<any> {
     const offset = (page - 1) * 20;
     const data = this.pokemonService.findAll(offset).pipe(
-      switchMap((response) =>
-        forkJoin(
+      /// get pokemon list
+      /// then use id in list to get pokemon detail
+      /// use switchMap to request parallel
+      /// use forkjoin to wait for last request complete
+      switchMap((response) => {
+        return forkJoin(
           response.map((i) => {
+            // console.log(i);
             const url = i.url;
             // get id param by regex
             const regex = /https\:\/\/pokeapi\.co\/api\/v2\/pokemon\/(\w+)/;
             const id = regex.exec(url)[1];
             return this.pokemonService.findOne(Number.parseInt(id));
           }),
-        ),
-      ),
+        );
+      }),
+      /// refactor result
+      map((pokemons: any[]) => {
+        const refactoredPokemons = pokemons.map((pokemon) => {
+          const { types, stats } = pokemon;
+          const newTypes = types.map((type) => {
+            return {
+              name: type.type.name,
+              url: type.type.url,
+            };
+          });
+          const newStats = stats.map((stat) => {
+            return {
+              ...stat,
+              name: stat.stat.name,
+            };
+          });
+          return {
+            ...pokemon,
+            types: newTypes,
+            stats: newStats,
+          };
+        });
+        return refactoredPokemons;
+      }),
     );
     return data;
   }
